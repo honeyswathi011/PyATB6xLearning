28a817a5941a64dba1c42340c1bdec37dea79716
swathi <honey.swathi011@gmail.com>
2025-12-12 16:51:10 +0530

    sample files pushing

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab004_Keyword.py
@@ -0,0 +1,18 @@
+# import keyword
+# print(keyword.kwlist)
+
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'async','await', 'break','class', 'continue',
+ 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
+ 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+age=20
+if age>=18:
+    print("Eligible for vote")
+else:
+    print("Not Eligible foe vote")
+    print("Not Eligible foe vote")
+    print("Not Eligible foe vote")
+
+
