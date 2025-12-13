96625e5d659f27e9255d7eeeb8396562c56fa8da
swathi <honey.swathi011@gmail.com>
2025-12-13 16:56:50 +0530

    sample files pushing

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab011_IQ_BODMAS.py
@@ -0,0 +1,10 @@
+a = 10+34*3-1
+print(a)
+
+# # Bodmas is an acronym that helps remember order of operations in mathematics
+# B: Brackets
+# O: order of the power or roots
+# D: Division
+# M: Multiplication
+# O: Addition
+# S: Subscription
